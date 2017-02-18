857c905bbe905ebc1fcc665b1deafffc7c601ff6
ArtyB <arthurbuldauskas@gmail.com>
2017-02-18 10:26:34 -0500

    add loader/webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,32 @@
 module.exports = {
   context: __dirname + '/src',
 
-  entry: './index.js',
+  entry: {
+    javascript: './index.js',
+    html: './index.html'
+  },
 
   output: {
     filename: 'bundle.js',
     path: __dirname + '/dist'
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json'],
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader']
+      },
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]',
+      }
+    ]
   }
 };
 
