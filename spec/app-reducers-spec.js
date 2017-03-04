51b6ef31713bbb5add9c452227ae299067264d03
Arty Buldauskas <abuldauskas@wayfair.com>
2017-03-04 14:54:02 -0500

    remove some tests

--- spec/app-reducers-spec.js
+++ spec/app-reducers-spec.js
@@ -1,17 +1,14 @@
 /* eslint-env jasmine */
-import reducerFactory from '../src/js/reducers/app-reducers';
+import reducer from '../src/js/reducers/app-reducers';
 import {
   is
 } from 'ramda';
 
-const isFunction = is(Function);
+const isObject = is(Object);
 
 describe('App Reducer', () => {
 
-  it('is a function', () => {
-    expect(isFunction(reducerFactory)).toBe(true);
-  });
-  it('returns a function', () => {
-    expect(isFunction(reducerFactory())).toBe(true);
+  it('is an Object', () => {
+    expect(isObject(reducer)).toBe(true);
   });
 });
