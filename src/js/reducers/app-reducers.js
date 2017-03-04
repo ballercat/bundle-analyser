5a7c2b86c32f39d6895ba0d9e443ad2f8f494ff7
Arty Buldauskas <abuldauskas@wayfair.com>
2017-03-03 22:07:38 -0500

    add watch:test

--- src/js/reducers/app-reducers.js
+++ src/js/reducers/app-reducers.js
@@ -1,1 +1,22 @@
+import {
+  Map,
+  List,
+  fromJS
+} from 'immutable';
+
+import {
+  curry
+} from 'ramda';
+
+const reducer = (state, action) {
+  switch(action.type) {
+    case FETCH_SCRIPT:
+    default:
+      return state;
+  };
+}
+
+export default (defaults) => {
+
+}
 
