cdb676fa31fdee45cdb22bf498d51eb25da61645
Arty Buldauskas <abuldauskas@wayfair.com>
2017-03-05 20:43:06 -0500

    module detail

--- src/js/d3/depTree.js
+++ src/js/d3/depTree.js
@@ -1,5 +1,7 @@
+import logger from '../logger';
 import * as d3 from 'd3';
 import R, {
+  is,
   all,
   not,
   isNil,
@@ -10,32 +12,85 @@ import R, {
   compose,
   pluck,
   type,
-  values
+  values,
+  addIndex
 } from 'ramda';
 
+const isArray = is(Array);
+const mapChild = curry((p, c, i) => ({
+    name: c.name,
+    size: c.size,
+    parent: p.name,
+    original: c,
+    children: c.expand ? addIndex(map)(mapChild(c), c.deps): null,
+    cid: i
+}));
 const parseData = data => ({
   name: data.name,
   size: data.size,
   parent: null,
-  children: data.deps.map(
-    (dep, idx) => ({
-      name: dep.name,
-      size: dep.size,
-      parent: data.name,
-      original: dep,
-      children: null,
-      cid: idx
-    }))
+  children: addIndex(map)(mapChild(data), data.deps)
 });
 
+// Collapse the node and all it's children
+const collapse = d => {
+  if(d.children) {
+    d._children = d.children
+    d._children.forEach(collapse)
+    d.children = null
+  }
+}
+
+// Toggle children on click.
+const click = curry((build, root, d) => {
+  if (d === root) {
+    if (d.children) {
+      d._children = d.children;
+      d.children = null;
+    } else {
+      d.children = d._children;
+      d._children = null;
+    }
+    build(d);
+  } else {
+    if (d.children) {
+      d.data.original.expand = false;
+    } else {
+      d.data.original.expand = true;
+    }
+    build(root);
+  }
+
+});
+
+const radius = d => Math.max((d.data || d).size, 1) * 2;
+const hierarchy = (data, width) => Object.assign(
+  d3.hierarchy(parseData(data), prop('children')),
+  {
+    isRoot: true,
+    x0: width/2,
+    y0: radius(data) * 1.5
+  }
+);
+const diagonal = d3.line().x(d => d.x).y(d => d.y); // .curve(d3.curveStep);
+
+const mapIds = addIndex((d, i) => d.id || i);
+const normalizeDepth = d => {
+  if (!d.isRoot) {
+    d.y = 50 + d.depth * 150 + (((d.data || d).size || 10) * 10);
+  } else {
+    d.y = radius(d) * 2;
+  }
+};
+const nodeTransform = curry((root, d) => d.parent ? `translate(${d.parent.x}, ${d.parent.y})` : `translate(${root.x0}, ${root.y0})`);
+
 export default (options) => {
- const {
+  const {
     width,
     height,
     data,
   } = options;
 
-  const radius = d => Math.max(d.data.size, 1) * 2;
   const target = document.createElement('div');
 
   const svg = d3.select(target)
@@ -44,39 +99,27 @@ export default (options) => {
     .attr('height', height + 20)
     .append('g');
 
-  const tree = d3.tree()
-		.size([width, height]);
-	var i = 0, duration = 750;
-  const root = d3.hierarchy(parseData(data), d => d.children);
-  root.isRoot = true;
-	root.x0 = width/2;
-	root.y0 = radius(root) * 1.5;
+  const tree = d3.tree().size([width, height]);
+  const duration = 750;
 
-  const build = (root) => {
-		root = tree(root);
+  const build = (data) => {
+		const root = tree(hierarchy(data, width));
+    const clickHandler = click(build, data);
     // Compute the new tree layout.
 		var nodes = root.descendants(),
 			links = root.descendants().slice(1);
 
     // Normalize for fixed-depth.
-    nodes.forEach(d => {
-      if (!d.isRoot) {
-        d.y = 50 + d.depth * 20 * Math.max(d.data.size, d.data.cid || 1);
-      } else {
-        d.y = radius(d) * 2;
-      }
-    });
-
+    nodes.forEach(normalizeDepth);
 
     // Update the nodes…
-    var node = svg.selectAll("g.node")
-      .data(nodes, d => d.id || (d.id = ++i));
+    var node = svg.selectAll("g.node").data(nodes, mapIds);
 
     // Enter any new nodes at the parent's previous position.
     var nodeEnter = node.enter().append("g")
       .attr("class", "node")
-      .attr("transform", () => `translate(${root.x0}, ${root.y0})`)
-      .on("click", click);
+      .attr("transform", nodeTransform(root))
+      .on("click", clickHandler);
 
     nodeEnter.append("circle")
       .attr("r", radius)
@@ -105,7 +148,7 @@ export default (options) => {
     // Transition exiting nodes to the parent's new position.
     var nodeExit = node.exit().transition()
       .duration(duration)
-      .attr("transform", d => `translate(${root.x},${root.y})`)
+      .attr("transform", nodeTransform(root))
       .remove();
 
     nodeExit.select("circle")
@@ -120,11 +163,11 @@ export default (options) => {
 
     // Enter any new links at the parent's previous position.
     var linkEnter = link.enter().insert("path", "g")
+      .style("stroke-dasharray", ("3, 5"))
       .attr("class", "link")
-      .attr("d", function(d) {
-        var o = {x: root.x0, y: root.y0};
-        return diagonal([o, o]);
-      });
+      .attr('d', d => d.parent ?
+        diagonal([d.parent, d.parent]) :
+        diagonal([{x: root.x0, y: root.y0}, {x: root.x0, y: root.y0}]));
 
 		var linkUpdate = linkEnter.merge(link);
 
@@ -136,10 +179,7 @@ export default (options) => {
     // Transition exiting nodes to the parent's new position.
     var linkExit = link.exit().transition()
       .duration(duration)
-      .attr("d", function(d) {
-        var o = {x: root.x, y: root.y};
-        return diagonal([o, o]);
-      })
+      .attr("d", (d) => d.parent ? diagonal([d.parent, d.parent]) : diagonal([root, root]))
       .remove();
 
     // Stash the old positions for transition.
@@ -149,34 +189,7 @@ export default (options) => {
     });
   };
 
-	// Collapse the node and all it's children
-	function collapse(d) {
-		if(d.children) {
-			d._children = d.children
-			d._children.forEach(collapse)
-			d.children = null
-		}
-	}
-
-  var diagonal = d3.line()
-    .x(d => d.x)
-    .y(d => d.y)
-    .curve(d3.curveStep);
-
-  // Toggle children on click.
-  function click(d) {
-    if (d.children) {
-      d._children = d.children;
-      d.children = null;
-    } else {
-      d.children = d._children;
-      d._children = null;
-    }
-    build(d);
-  }
-
-	root.children.forEach(collapse);
-	build(root);
+	build(data);
 
   return target;
 };
--- src/js/logger.js
+++ src/js/logger.js
@@ -6,6 +6,7 @@ import {
 export default (konsole = global.console) => ({
   log: tap(konsole.log),
   logBefore: fn => compose(fn, konsole.log),
+
   logAfter: fn => compose(konsole.log, fn)
 });
 
